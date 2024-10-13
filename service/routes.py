c67da725d11c440766277dfb922c71e8a08f8469
ananya-muralidhar <ananyamraju2000@gmail.com>
2024-10-13 19:01:44 +0000

    Added delete customer endpoint

--- service/routes.py
+++ service/routes.py
@@ -137,6 +137,28 @@ def update_customers(customer_id):
 
 
 ######################################################################
+# DELETE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["DELETE"])
+def delete_customers(customer_id):
+    """
+    Delete a Customer
+
+    This endpoint will delete a Customer based the id specified in the path
+    """
+    app.logger.info("Request to Delete a customer with id [%s]", customer_id)
+
+    # Delete the Customer if it exists
+    customer = Customer.find(customer_id)
+    if customer:
+        app.logger.info("Customer with ID: %d found.", customer.id)
+        customer.delete()
+
+    app.logger.info("Customer with ID: %d delete complete.", customer_id)
+    return {}, status.HTTP_204_NO_CONTENT
+
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
